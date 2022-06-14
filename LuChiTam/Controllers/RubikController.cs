f77002bcb01e6e8649a4119d9a23e1a3627b5544
CHITAM <chitamlu38@gmail.com>
2022-06-14 15:55:42 +0700

    Add project files.

---
+++ LuChiTam/Controllers/RubikController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LuChiTam.Models;
+
+namespace LuChiTam.Controllers
+{
+    public class RubikController : Controller
+    {
+        // GET: Rubik
+        MyData data = new MyData();
+        public ActionResult Index()
+        {
+            var all_rubik = from s in data.Rubiks select s;
+            return View(all_rubik);
+        }
+    }
+}
